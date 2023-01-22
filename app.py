fa1fbe869125aaa05869c2648bed9bbac7220b5f
tsw303005 <tsw303005@gmail.com>
2023-01-22 16:07:05 +0800

    linebot
    1. add linebot
    2. add command to serve request

--- app.py
+++ app.py
@@ -3,28 +3,46 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from dotenv import load_dotenv
-from src.pokernow import Pokernow
+from src.command import Command
 import os
 
 app = Flask(__name__)
+load_dotenv()
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRETE'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    resp = command.makeCommand(event.message.text, line_bot_api,  event)
+
+    if resp != 'return':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=resp))
+
 
 @app.route('/', methods=['GET'])
 def status():
     return "status: ok"
 
-@app.route('/getScore', methods=['POST'])
-def getScore():
-    url = request.get_json()['url']
-    print(url)
-    result = pokernow.getScore(url)
-    
-    return result
-
 
 if __name__ == '__main__':
-    load_dotenv()
-    pokernow = Pokernow()
-    line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
-    handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRETE'))
+    command = Command()
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="3000", debug=True)
